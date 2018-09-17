bd0fb110d93ac649f19e1a5b4c680da31a74a100
Frodos <f.mushenok@exeter.ac.uk>
2018-09-18 00:51:09 +0300

    Add RLE converter

--- my_rlc.py
+++ my_rlc.py
@@ -11,8 +11,34 @@ def my_rlc(arr):
     d = np.diff(np.int8(np.greater(arr, 0)))
     starts = np.where(d == 1)[0]
     ends = np.where(d == -1)[0]
-    return np.stack((starts, ends - starts), axis=1).flatten()
+    return np.stack((starts+1, ends - starts), axis=1).flatten()
 
+def mask_to_rle(mask):
+    mask_flat = mask.flatten('F')
+    flag = 0
+    rle_list = list()
+    for i in range(mask_flat.shape[0]):
+        if flag == 0:
+            if mask_flat[i] == 1:
+                flag = 1
+                starts = i+1
+                rle_list.append(starts)
+        else:
+            if mask_flat[i] == 0:
+                flag = 0
+                ends = i
+                rle_list.append(ends-starts+1)
+    if flag == 1:
+        ends = mask_flat.shape[0]
+        rle_list.append(ends-starts+1)
+    #sanity check
+    if len(rle_list) % 2 != 0:
+        print('NG')
+    if len(rle_list) == 0:
+        rle = np.nan
+    else:
+        rle = ' '.join(map(str,rle_list))
+    return rle
 
 def main():
     """
@@ -23,12 +49,14 @@ def main():
         [0, 0, 0, 1, 1, 1, 0],
         [0, 0, 0, 1, 0, 1, 0],
         [0, 0, 0, 0, 0, 0, 0],
-        [1, 0, 1, 0, 1, 0, 1]
+        [1, 0, 1, 0, 1, 0, 0]
     ]
+    test = np.array(tests)
+    v1 = my_rlc(test.flatten(order='F'))
+    print(v1)
 
-    for test_arr in tests:
-        v = my_rlc(np.array(test_arr))
-        print(v)
+    v2 = mask_to_rle(test)
+    print(v2)
 
 if __name__ == '__main__':
     main()
--- prepare_submit.py
+++ prepare_submit.py
@@ -14,7 +14,9 @@ def my_rlc(arr):
     d = np.diff(np.int8(np.greater(arr, 0)))
     starts = np.where(d == 1)[0]
     ends = np.where(d == -1)[0]
-    return np.stack((starts, ends - starts), axis=1).flatten()
+    len = ends - starts
+    assert (starts.shape == ends.shape)
+    return np.stack((starts+1, len), axis=1).flatten()
 
 def main(pth=None):
     """
@@ -22,13 +24,16 @@ def main(pth=None):
     :param pth:
     :return:
     """
-    s = ''
     with open("Output.txt", "w") as text_file:
+        print('id,rle_mask', file=text_file)
         for file in os.listdir(pth):
             file_name, _ = os.path.splitext(file)
-            img = cv2.imread(os.path.join(os.path.join(pth, file)), -1)
-            s = np.array_str(my_rlc(img))
-            print("{}, {}".format(file_name, re.sub(' +', ' ', s[1:-1])), file=text_file)
+            img = cv2.imread(os.path.join(os.path.join(pth, file)), -1).flatten(order='C')
+            s = np.array_str(my_rlc(img), max_line_width=99999)[1:-1]
+            if s == '':
+                print(file_name + ',', file=text_file)
+            else:
+                print(file_name+','+re.sub(' +', ' ', s.strip()), file=text_file)
 
 
 if __name__ == '__main__':
